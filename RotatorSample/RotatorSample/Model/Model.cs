8b53c65f70b9a1fac9c99764c8f9fabb240632d1
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-26 18:40:41 +0530

    Rotator sample has been added

---
+++ RotatorSample/RotatorSample/Model/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RotatorSample
+{
+    public class RotatorModel
+    {
+        public RotatorModel(string imagestr)
+        {
+            Image = imagestr;
+        }
+        private String _image;
+        public String Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+    }
+}
---
+++ RotatorSample/RotatorSample/ViewModel/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RotatorSample
+{
+    public class RotatorViewModel
+    {
+        public RotatorViewModel()
+        {
+            ImageCollection.Add(new RotatorModel("movie1.png"));
+            ImageCollection.Add(new RotatorModel("movie2.png"));
+            ImageCollection.Add(new RotatorModel("movie3.png"));
+            ImageCollection.Add(new RotatorModel("movie4.png"));
+            ImageCollection.Add(new RotatorModel("movie5.png"));
+        }
+        private List<RotatorModel> imageCollection = new List<RotatorModel>();
+
+        public List<RotatorModel> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value; }
+        }
+    }
+}
